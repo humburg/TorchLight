fb41d48efcdadca3a2663bc48d5be19f709ee33f
Peter Humburg <peter.humburg@gmail.com>
2021-12-20 21:11:18 +1100

    Amend license notice

--- torchlight.js
+++ torchlight.js
@@ -16,6 +16,12 @@
  * only if you promise to buy one for Stephen and Philippe as well.
  * Alan Davies
  * ----------------------------------------------------------------------------
+ * <peter.humburg@gmail.com> updated this file.  As long as you retain this
+ * notice you can do whatever you want with this stuff.  If we meet some day,
+ * and you think this stuff is worth it, you can buy me a beer in return, but
+ * only if you promise to buy one for Stephen and Philippe as well.
+ * Alan Davies
+ * ----------------------------------------------------------------------------
  */
 
 class TorchLight {
